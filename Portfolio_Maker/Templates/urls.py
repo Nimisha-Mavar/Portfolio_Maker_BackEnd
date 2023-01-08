910c767066cff7b5266034173932140d03aa1b3d
Nimisha Mavar <nimishamavar.0101@gmail.com>
2023-01-07 20:28:36 -0500

    Nimisha/Templates/add template detail page

--- Portfolio_Maker/Templates/urls.py
+++ Portfolio_Maker/Templates/urls.py
@@ -3,7 +3,8 @@ from django.urls import URLPattern,path
 from . import views
 
 urlpatterns = [
-   path('temp_list',views.temp_list)
+   path('temp_list',views.temp_list),
+   path('temp_detail',views.temp_detail)
 ]
 
 
