af6b9873f52846b1fc28f3fbc1dfd306a511552f
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2023-01-13 10:58:33 -0500

    Nimisha/Scanner/Load scanner page

--- Portfolio_Maker/Scanner/urls.py
+++ Portfolio_Maker/Scanner/urls.py
@@ -3,5 +3,5 @@ from django.urls import URLPattern,path
 from . import views
 
 urlpatterns = [
-    
+    path('scanner',views.scanner_page)
 ]
--- Portfolio_Maker/Scanner/views.py
+++ Portfolio_Maker/Scanner/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def scanner_page(request):
+    return render(request,'Scanner.html')
