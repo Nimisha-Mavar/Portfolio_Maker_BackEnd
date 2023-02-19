52257a43078826e027269754b3ee6aa387863e93
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2023-02-18 23:22:48 -0500

    Nimisha/Templates/Create offer and  template detail model

--- Portfolio_Maker/Templates/admin.py
+++ Portfolio_Maker/Templates/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Offer,Detail
+from django.contrib.admin.sites import site
 # Register your models here.
+admin.site.register(Offer),
+admin.site.register(Detail)
--- Portfolio_Maker/Templates/models.py
+++ Portfolio_Maker/Templates/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+class Offer(models.Model):
+    Offer_id=models.IntegerField(primary_key=True)
+    Offer=models.BooleanField(default=False)
+    Offer_value=models.IntegerField()
+    Start_date=models.DateField()
+    End_date=models.DateField()
+
 # Create your models here.
+class Detail(models.Model):
+    Temp_Cat=(
+        ('P','Portfolio'),
+        ('R','Resume'),
+        ('A','ATS')
+    )
+    Temp_Type=(
+        ('B','Basic'),
+        ('P','Premium')
+    )
+    Temp_name=models.CharField(max_length=100)
+    Temp_cat=models.CharField(max_length=30,default="Portfolio",choices=Temp_Cat)
+    Temp_type=models.CharField(max_length=30,default="Basic",choices=Temp_Type)
+    Temp_date=models.DateField()
+    Temp_img1=models.ImageField(upload_to='Temp_Imgs')
+    Temp_img2=models.ImageField(upload_to='Temp_Imgs',null=True)
+    Temp_img3=models.ImageField(upload_to='Temp_Imgs',null=True)
+    Temp_des=models.TextField()
+    Temp_price=models.IntegerField()
+    Temp_file=models.FileField(upload_to='Template')
+    Temp_active=models.BooleanField(default=False)
+    Temp_offer=models.ForeignKey('Offer',default=0,on_delete=models.SET_DEFAULT)
