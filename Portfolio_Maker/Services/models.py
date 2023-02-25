31baa7b7c6d33869203edf1c57be01178b58f384
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2023-02-25 11:01:35 -0500

    Nimisha/Services/Create Personal_info model

--- Portfolio_Maker/Services/models.py
+++ Portfolio_Maker/Services/models.py
@@ -13,3 +13,16 @@ class Resume(models.Model):
     Resume_id=models.IntegerField(primary_key=True)
     Template=models.ForeignKey(Detail,default=0,on_delete=models.CASCADE)
     User=models.ForeignKey(User,default=0,on_delete=models.CASCADE)
+
+#Personal Information
+class Personal_info(models.Model):
+    Personal_id=models.IntegerField(primary_key=True)
+    Resume=models.ForeignKey(Resume,default=0,on_delete=models.CASCADE)
+    Portfolio=models.ForeignKey(Portfolio,default=0,on_delete=models.CASCADE)
+    First_name=models.CharField(max_length=30)
+    Last_name=models.CharField(max_length=30)
+    Address=models.TextField()
+    Phone=models.CharField(max_length=10)
+    Email=models.EmailField()
+    Dob=models.DateField()
+    Philosophy=models.TextField()
