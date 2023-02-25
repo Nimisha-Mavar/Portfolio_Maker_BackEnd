ca5091289c3e59b74ef3603a3aacfedc0c078bcb
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2023-02-25 10:39:35 -0500

    Nimisha/Services/Create Portfolio model

--- Portfolio_Maker/Services/models.py
+++ Portfolio_Maker/Services/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from Templates.models import Detail
+from django.contrib.auth.models import User, auth
 # Create your models here.
+#Portfolio model
+class Portfolio(models.Model):
+    Portfolio_id=models.IntegerField(primary_key=True)
+    Template=models.ForeignKey(Detail,default=0,on_delete=models.CASCADE)
+    User=models.ForeignKey(User,default=0,on_delete=models.CASCADE)
