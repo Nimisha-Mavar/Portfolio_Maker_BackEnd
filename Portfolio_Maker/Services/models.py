51ed855ac54f083d050ce8ec0c38a50c7340fe20
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2023-02-25 20:02:50 -0500

    Nimisha/Services/Create Experience model

--- Portfolio_Maker/Services/models.py
+++ Portfolio_Maker/Services/models.py
@@ -27,6 +27,7 @@ class Personal_info(models.Model):
     Dob=models.DateField()
     Philosophy=models.TextField()
 
+#educaion model
 class Education(models.Model):
     Education_id=models.IntegerField(primary_key=True)
     Resume=models.ForeignKey(Resume,default=0,on_delete=models.CASCADE)
@@ -34,4 +35,19 @@ class Education(models.Model):
     Institute=models.CharField(max_length=50)
     Degree=models.CharField(max_length=50)
     Start_year=models.CharField(max_length=10)
-    End_year=models.CharField(max_length=10)
+    End_year=models.CharField(max_length=10,default='no')
+    Current=models.BooleanField(default=False)
+
+#Experience model
+class Experience(models.Model):
+    Experience_id=models.IntegerField(primary_key=True)
+    Resume=models.ForeignKey(Resume,default=0,on_delete=models.CASCADE)
+    Portfolio=models.ForeignKey(Portfolio,default=0,on_delete=models.CASCADE)
+    Company=models.CharField(max_length=50)
+    Role=models.CharField(max_length=30)
+    Start_year=models.CharField(max_length=10)
+    End_year=models.CharField(max_length=10,default='no')
+    Current=models.BooleanField(default=False)
+
+
+
