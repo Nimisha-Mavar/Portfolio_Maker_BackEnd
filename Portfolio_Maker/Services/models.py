7217b6a8eba7caf8f53366560fb80d412b716fb7
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2023-02-25 20:16:27 -0500

    Nimisha/Services/Create Project and Skill model

--- Portfolio_Maker/Services/models.py
+++ Portfolio_Maker/Services/models.py
@@ -49,5 +49,24 @@ class Experience(models.Model):
     End_year=models.CharField(max_length=10,default='no')
     Current=models.BooleanField(default=False)
 
+#Project model
+class Project(models.Model):
+    Project_id=models.IntegerField(primary_key=True)
+    Resume=models.ForeignKey(Resume,default=0,on_delete=models.CASCADE)
+    Portfolio=models.ForeignKey(Portfolio,default=0,on_delete=models.CASCADE)
+    Title=models.CharField(max_length=50)
+    Start_year=models.CharField(max_length=10)
+    End_year=models.CharField(max_length=10,default='no')
+    Current=models.BooleanField(default=False)
+    Description=models.TextField(default="NO")
+
+#Skill model
+class Skill(models.Model):
+    Skill_id=models.IntegerField(primary_key=True)
+    Resume=models.ForeignKey(Resume,default=0,on_delete=models.CASCADE)
+    Portfolio=models.ForeignKey(Portfolio,default=0,on_delete=models.CASCADE)
+    Name=models.CharField(max_length=30)
+    Level=models.CharField(max_length=20)
+
 
 
