122d9a7ca23f007f7ec2d71968a762f951e9a69c
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2023-02-26 12:49:32 -0500

    Nimisha/Create Master page

--- Portfolio_Maker/Index/urls.py
+++ Portfolio_Maker/Index/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home),
-    path('faq',views.faq),
+    path('faq',views.faq,name="faq"),
     path('contact1',views.contact1,name='contact1')
 ]
