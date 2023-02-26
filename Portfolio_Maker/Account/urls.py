1743713b19d59f392aaf60740ad65bfc97c70bd2
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2023-02-26 13:21:00 -0500

    Nimisha/Accounts/code for Logout
    Add the request in dashboard's logout button

--- Portfolio_Maker/Account/urls.py
+++ Portfolio_Maker/Account/urls.py
@@ -2,6 +2,7 @@ from django.urls import URLPattern,path
 from . import views
 
 urlpatterns = [
-    path('login',views.login ,name="login"),
-    path('Register',views.Register, name="Register")
+    path('login',views.login ,name="Login"),
+    path('Register',views.Register, name="Register"),
+    path('Logout',views.logout,name="Logout")
 ]
