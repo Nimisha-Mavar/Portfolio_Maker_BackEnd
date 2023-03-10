40b14bc14e01ae1087dfd9dd260c1ec56937145f
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2023-03-10 17:53:26 -0500

    Nimisha/Account/Chnage password logic
    in Dashboard page

--- Portfolio_Maker/Account/urls.py
+++ Portfolio_Maker/Account/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('Register',views.Register, name="Register"),
     path('Logout',views.logout,name="Logout"),
     path('forgot_password',views.forgot_password,name="forgot_password"),
-    path('set_pass', views.set_pass, name="set_pass")
+    path('set_pass', views.set_pass, name="set_pass"),
+    path('change_pass',views.cng_pass,name="change_pass")
 ]
--- Portfolio_Maker/Account/views.py
+++ Portfolio_Maker/Account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
-
+from django.contrib.auth.hashers import check_password
 # Create your views here.
 
 def Register(request):
@@ -60,7 +60,7 @@ def forgot_password(request):
     else:
         return render(request, 'forgot_password.html')
 
-
+#logic of forgot password
 def set_pass(request):
     npass=request.POST['newpass']
     cpass=request.POST['cpass']
@@ -73,3 +73,18 @@ def set_pass(request):
     else:
         return render(request, 'forgot_pass1.html')
 
+#logic for change password
+def cng_pass(request):
+    npass=request.POST['npass']
+    cpass=request.POST['cpass']
+    u_id=request.user
+    u = User.objects.get(id=u_id.id)
+    print(u.username)
+    print(u.password)
+    if check_password(cpass,u.password):
+        u.set_password(npass)
+        u.save()
+        logout(request)
+        return redirect('Login')
+    else:
+        return render(request,'dashboard.html',{'msg':"Password does not match.."})
