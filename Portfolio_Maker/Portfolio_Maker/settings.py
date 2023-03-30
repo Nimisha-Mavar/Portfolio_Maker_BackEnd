e3c1f8fbbf4f78fafee7d12cda418f20704987dc
Anjali Zala <anjalizala13@gmail.com>
2023-03-30 14:10:02 +0530

    Anjali/Update_database_settings

--- Portfolio_Maker/Portfolio_Maker/settings.py
+++ Portfolio_Maker/Portfolio_Maker/settings.py
@@ -87,9 +87,9 @@ WSGI_APPLICATION = 'Portfolio_Maker.wsgi.application'
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "main_db",
+        "NAME": "Portfolio_Maker",
         "USER": "postgres",
-        "PASSWORD":"anjali1309",
+        "PASSWORD":"PG12345",
         "HOST":"localhost"
     }
 }
