4426a5645efeb8ddf8549883d8fe10903f0574ad
Nimisha Mavar <112267753+Nimisha-Mavar@users.noreply.github.com>
2023-04-06 23:50:49 -0400

    Nimisha/Write two functions for retrieve Resume and Portfolio data
    1. Portfolio_data, 2. Resume_data

--- Portfolio_Maker/Template_Preview/views.py
+++ Portfolio_Maker/Template_Preview/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from Services.models import *
+from payment.models import Payment
 # Portfolio Templates
 def Basic_pf_1(request):
     return render(request,'Basic_Pf_1.html')
@@ -17,6 +18,53 @@ def iportfolio(request):
     if request.method=='GET':
         pid=request.GET['pid']
         u_id=request.user
+        contaxt=Portfolio_data(pid,u_id)
+        return render(request,'Premium_Pf_3.html',contaxt)
+    else:
+        return render(request,'D_Premium_Pf_3.html')
+
+# Resume Template
+def Basic_Rm_1(request):
+    if request.method=='GET':
+        pid=request.GET['pid']
+        u_id=request.user
+        contaxt=Resume_data(pid,u_id)
+        return render(request,'Basic_Resume_1.html',contaxt)
+    else:
+        return render(request,'Basic_Resume_1.html')
+
+def Basic_Rm_2(request):
+    return render(request,'Premium_R_2.html')
+
+def Premium_Rm_1(request):
+    return render(request,'Premium_R_1.html')
+
+def Premium_Rm_2(request):
+    if request.method=='GET':
+        pid=request.GET['pid']
+        u_id=request.user
+        contaxt=Resume_data(pid,u_id)
+        return render(request,'Premium_R_3.html',contaxt)
+    else:
+        return render(request,'Premium_R_3.html')
+
+def Resume_data(pid,u_id):
+        prs=Personal_info.objects.filter(User_id=u_id.id)
+        edu=Education.objects.filter(Resume_id=pid)
+        ex=Experience.objects.filter(Resume_id=pid)
+        proj=Project.objects.filter(Resume_id=pid)
+        skill=Skill.objects.filter(Resume_id=pid)
+        contaxt={
+            'prs':prs,
+            'edu':edu,
+            'ex':ex,
+            'pro':proj,
+            'sk':skill,
+            'active':True
+        }
+        return contaxt
+
+def Portfolio_data(pid,u_id):
         prs=Personal_info.objects.filter(User_id=u_id.id)
         edu=Education.objects.filter(Portfolio_id=pid)
         ex=Experience.objects.filter(Portfolio_id=pid)
@@ -33,37 +81,4 @@ def iportfolio(request):
             'awd':award,
             'scl':social
         }
-        return render(request,'Premium_Pf_3.html',contaxt)
-    else:
-        return render(request,'D_Premium_Pf_3.html')
-
-# Resume Template
-def Basic_Rm_1(request):
-    if request.method=='GET':
-        pid=request.GET['pid']
-        u_id=request.user
-        prs=Personal_info.objects.filter(User_id=u_id.id)
-        edu=Education.objects.filter(Resume_id=pid)
-        ex=Experience.objects.filter(Resume_id=pid)
-        proj=Project.objects.filter(Resume_id=pid)
-        skill=Skill.objects.filter(Resume_id=pid)
-        contaxt={
-            'prs':prs,
-            'edu':edu,
-            'ex':ex,
-            'pro':proj,
-            'sk':skill,
-            'active':True
-        }
-        return render(request,'Basic_Resume_1.html',contaxt)
-    else:
-        return render(request,'Basic_Resume_1.html')
-
-def Basic_Rm_2(request):
-    return render(request,'Premium_R_2.html')
-
-def Premium_Rm_1(request):
-    return render(request,'Premium_R_1.html')
-
-def Premium_Rm_2(request):
-    return render(request,'Premium_R_3.html')
+        return contaxt
